1fcb5f21405af11fe8aa114777987e7344124dce
Steve Noel <30878244+StevenNoel@users.noreply.github.com>
2018-07-13 16:06:38 -0500

    Script Heading Template
    This contains a default template to use at the top of all scripts. To explain purpose, details, and examples of how the script works and how to interact with it.

---
+++ ScriptHeadingTemplate.cmd
@@ -0,0 +1,32 @@
+#requires -version 3.0
+
+<#
+    (Summarize purpose of the script)
+    (Name of Creator)
+    (Date Created)
+    (Modification history:)
+    (Date and Change Description)
+#>
+
+<#
+.SYNOPSIS
+(Summary of Script)
+
+.DESCRIPTION
+.PARAMETER (parameter1)
+(Enter details on how paraemter works/requirements)
+
+.PARAMETER (parameter2)
+(Enter details on how paraemter works/requirements)
+
+.EXAMPLE
+& '.\(Name of Script).ps1' -(Parameter1)
+(Enter Details on what the script will do with parameter details)
+
+.EXAMPLE
+& '.\(Name of Script).ps1' -(Parameter1) -(Paraemter2)
+(Enter Details on what the script will do with parameter details)
+
+.NOTES
+(Enter specific things to note for this script)
+#>
